6060f52fcb937ab6b35921a2771afa0483aec9a0
moshecc <moshec545@gmail.com>
2023-01-22 13:29:04 +0200

    Log in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HomePage from './components/HomePage/HomePage'
+import Login from './components/LogIn'
 
 export default function App() {
   return (
     <>
-    <HomePage/>
-    <div>hayim halle</div>
+    {/* <HomePage/> */}
+    <Login/>
     </>
   )
 }
