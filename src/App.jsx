662c6bf2e7d8bafcb4352505ca531f21f2fdc9de
moshecc <moshec545@gmail.com>
2023-01-22 10:07:40 +0200

    mo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomePage from './components/HomePage/HomePage'
+
+export default function App() {
+  return (
+    <>
+    <HomePage/>
+    </>
+  )
+}
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HomePage.css";
+export default function HomePage() {
+  return (
+    <>
+      <div className="bgImg justify-content-center d-flex">HomePage</div>
+    </>
+  );
+}
