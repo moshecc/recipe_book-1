1ecbd6bf72aff5ce902e4de16b253ddfcf78d952
haim hale <hayim9939@gmail.com>
2023-01-22 13:53:51 +0200

    homepage wite login

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Login from "../Log in/Login";
 import "./HomePage.css";
-import ""
 export default function HomePage() {
   return (
     <>
       <div className="bgImg">
         <img className="position-absolute" src="../../imges/logo.png" alt="" />
         <h1 className="myFont">המתכונים שלי</h1>
-<<<<<<< HEAD
-        <div className="h-50">
+        <div className="h-50 pt-4">
+          <Login/>
         </div>
-
-=======
-        <div className="h-50"></div>
-        <Login/>
->>>>>>> 22de30ab4519a83f391c992f460bd28fe01fda7d
+       
         <div className=" justify-content-center d-flex">
           <span>
             <Link className="nav-link active">about us</Link>
