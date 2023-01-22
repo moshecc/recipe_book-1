eafd099a4aaad097473585e434959a3e3f2ff686
haim hale <hayim9939@gmail.com>
2023-01-22 13:40:28 +0200

    hayim

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import HomePage from './components/HomePage/HomePage'
-import Login from './components/LogIn'
+
 
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-    {/* <HomePage/> */}
-    <Login/>
-=======
     <HomePage/>
->>>>>>> e3db04f7bb8f4e6a916f6adfcf987b8b18b8b199
     </>
   )
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import ""
 export default function HomePage() {
   return (
     <>
       <div className="bgImg">
         <img className="position-absolute" src="../../imges/logo.png" alt="" />
         <h1 className="myFont">המתכונים שלי</h1>
-        <div className="h-50"></div>
+        <div className="h-50">
+        </div>
 
         <div className=" justify-content-center d-flex">
           <span>
